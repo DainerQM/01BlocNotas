5a2eddc64e2c291d690d9b192ccb114a4b6bbab5
DainerQM <64506635+DainerQM@users.noreply.github.com>
2024-06-24 18:48:13 -0500

    Add files via upload

---
+++ PPLU18/main.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+
+palabra = input('Ingrese una palabra: ')
+
+if palabra in meme_dict:
+    print(meme_dict[palabra])
+
